99abbf52799263c9c7ad86ea01a29fefc0a37756
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2019-03-11 14:39:07 +0200

    init commit

---
+++ DomainOfTheSaneWizard/Assets/PlayerMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+    private Rigidbody2D rb2D;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start() {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        //horizontal < > movement:
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+        //vertical ^ v movement:
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+        }
+    }
+}
